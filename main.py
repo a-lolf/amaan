6567c3b74b996ff46c8f2080674c72f177ad4a05
amaan ansari <ansari.amaan@outlook.com>
2024-12-25 17:36:46 +0530

    commented host

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=80)
 
 
 # @app.route('/hello/')
